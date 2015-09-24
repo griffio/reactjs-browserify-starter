32a8169497df6ce25e781e7c010e0b1aedab8021
griffio <griffio@users.noreply.github.com>
2015-09-24 20:56:03 +0100

    deps

--- index.jsx
+++ index.jsx
@@ -1,4 +1,6 @@
 var React = require("react");
 var Counter = require("./counter.jsx");
 var counter = {count: 42};
+React.render(React.createElement('h1', null, 'Counter'), document.body);
 React.render(<Counter initialCounter={counter} />, document.body);
+
