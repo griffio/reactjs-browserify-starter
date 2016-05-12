720860e4c20dbfdaf311473a64d75f72a0f79669
griffio <griffio@users.noreply.github.com>
2016-05-12 15:06:07 +0100

    deprecations
    getDOMNode -> findDOMNode or use refs

--- test.jsx
+++ test.jsx
@@ -1,5 +1,6 @@
 var Tape = require('tape');
 var React = require('react');
+var ReactDOM = require('react-dom');
 var ReactTestUtils = require('react-addons-test-utils');
 var Counter = require('./counter.jsx');
 
@@ -7,12 +8,12 @@ var result = {count: 42};
 
 Tape('Counter', function (t) {
 
-    t.plan(1);
+  t.plan(1);
 
-    var counter = ReactTestUtils.renderIntoDocument(<Counter initialCounter={result} />);
+  var counter = ReactTestUtils.renderIntoDocument(<Counter refs="counter" initialCounter={result}/>);
 
-    ReactTestUtils.Simulate.click(counter.getDOMNode());
+  ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(counter));
 
-    t.equal(counter.state.count, result.count +1, 'increment counter once');
+  t.equal(counter.state.count, result.count + 1, 'increment counter once');
 
 });
