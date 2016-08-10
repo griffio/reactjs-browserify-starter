52ca441d0759c4fa651f9920b1405b8830fa53e1
griffio <griffio@users.noreply.github.com>
2016-08-10 17:03:06 +0100

    adds Aphrodite inline CSS

--- counter.jsx
+++ counter.jsx
@@ -13,9 +13,24 @@ export default class Counter extends Component {
     }
 
 	render() {
-		return <button onClick={this.clickHandler}>{this.state.count}</button>;
+		return <button className={css(styles.action, styles.larger)} onClick={this.clickHandler}>{this.state.count}</button>;
 	}
 };
 
 Counter.propTypes = {initialCounter: React.PropTypes.number};
 Counter.defaultProps = {initialCounter: 0};
+
+const styles = StyleSheet.create({
+
+	action: {
+		color: 'white',
+		borderRadius: '4px',
+		textShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+		background: 'rgb(28, 184, 65)'
+	},					 
+
+	larger: {
+		fontSize: '120%'
+	}
+
+});
