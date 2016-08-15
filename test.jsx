9472f258517e834391de5b74b2d66f2250bf8803
griffio <griffio@users.noreply.github.com>
2016-08-15 14:47:36 +0100

    organize test asserts

--- test.jsx
+++ test.jsx
@@ -5,28 +5,32 @@ import {mount} from 'enzyme';
 import ReactTestUtils from 'react-addons-test-utils';
 import Counter from './counter.jsx';
 
-const initialCount = 42;
-const expectedCount = initialCount + 1;
-
 Tape('<Counter /> React Test Utils', (t) => {
 
   t.plan(1);
 
-  const counter = ReactTestUtils.renderIntoDocument(<Counter refs="counter" initialCounter={initialCount}/>);
+  const counter = ReactTestUtils.renderIntoDocument(<Counter refs="counter" initialCounter={42}/>);
+
+  const expected = counter.state.count + 1
 
   ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(counter));
 
-  t.equal(counter.state.count, expectedCount, 'increment the counter once');
+  t.equal(counter.state.count, expected, 'incremented the counter once');
 
+  t.end()
 });
 
 Tape('<Counter /> enzyme mount wrapper simulate state', (t) => {
 
-	t.plan(2);
+  t.plan(1);
 
-	const wrapper = mount(<Counter refs="counter" initialCounter={initialCount} />);
+  const wrapper = mount(<Counter refs="counter" />);
 
-	wrapper.simulate('click');
+  const expected = wrapper.state().count + 1
 
-	t.equal(wrapper.state().count, expectedCount, 'increment the counter once');
+  wrapper.simulate('click');
+
+  t.equal(wrapper.state().count, expected, 'increment the counter once');
+
+  t.end()
 });
